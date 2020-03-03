573922cddc9b755f4735d65198d0e2b80bd706db
SIGGA\fagner.gomes <fagner.gomes@sigga.com>
2020-03-03 17:45:39 -0300

    Created user  migration

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,10 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'byexpense',
+  database: 'gobarber',
   define: {
     timestemps: true, // define que vou ter uma coluna created_at
     underscored: true,
-    underscoredAll: true // Garante que quero usar padão underscore nas tabelas e colunas.
-  }
+    underscoredAll: true, // Garante que quero usar padão underscore nas tabelas e colunas.
+  },
 };
